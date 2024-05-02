097d18be82dbb260df42a17bce8564bcb916459e
LaraCL <152405898+LaraCL@users.noreply.github.com>
2024-05-02 21:02:31 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ let classifier;
 let img;
 let resultDiv;
 let classifyButton;
+let correctButton;
+let incorrectButton;
 
 function setup() {
   createCanvas(400, 400);
@@ -15,9 +17,15 @@ function setup() {
   // Ergebnisanzeige erstellen
   resultDiv = select('#result');
 
-  // Button zum manuellen Starten der Klassifizierung
+  // Buttons für Klassifizierung
   classifyButton = select('#classifyButton');
   classifyButton.mousePressed(classifyImage);
+  
+  correctButton = select('#correctButton');
+  correctButton.mousePressed(() => { classifyImage(true); });
+  
+  incorrectButton = select('#incorrectButton');
+  incorrectButton.mousePressed(() => { classifyImage(false); });
 
   // Image Classifier mit MobileNet initialisieren
   classifier = ml5.imageClassifier('MobileNet', () => {
@@ -52,32 +60,29 @@ function gotFile(file) {
   }
 }
 
-function classifyImage() {
+function classifyImage(isCorrect = null) {
   if (img) {
-    classifier.classify(img.elt, gotResult);
+    classifier.classify(img.elt, (error, results) => {
+      if (error) {
+        console.error(error);
+      } else {
+        // Ergebnis anzeigen
+        resultDiv.html(`<strong>Label:</strong> ${results[0].label}<br><strong>Confidence:</strong> ${nf(results[0].confidence, 0, 2)}`);
+
+        // Thumbnail im #thumbnail-Bereich anzeigen
+        let thumbnailElement = select('#thumbnail');
+        thumbnailElement.html(''); // Vorhandenes Inhalt löschen
+        img.show(); // Bild sichtbar machen
+        img.size(200, 200); // Größe festlegen
+        img.parent('thumbnail'); // Bild in #thumbnail-Bereich einfügen
+
+        // Speichern der Klassifizierung
+        if (isCorrect !== null) {
+          saveClassification(isCorrect, results[0].label, results[0].confidence);
+        }
+      }
+    });
   } else {
     console.log('Es wurde noch kein Bild hochgeladen.');
-  }
-}
-
-function gotResult(error, results) {
-  if (error) {
-    console.error(error);
-  } else {
-    // Ergebnis anzeigen
-    resultDiv.html(`<strong>Label:</strong> ${results[0].label}<br><strong>Confidence:</strong> ${nf(results[0].confidence, 0, 2)}`);
-
-    // Thumbnail im #thumbnail-Bereich anzeigen
-    if (img) {
-      let thumbnailElement = select('#thumbnail');
-      thumbnailElement.html(''); // Vorhandenes Inhalt löschen
-      img.show(); // Bild sichtbar machen
-      img.size(200, 200); // Größe festlegen
-      img.parent('thumbnail'); // Bild in #thumbnail-Bereich einfügen
-
-      // Speichern der Klassifizierung
-      let isCorrect = confirm('Ist die Klassifizierung korrekt?');
-      saveClassification(isCorrect, results[0].label, results[0].confidence);
-    }
   }
 }
