3a1a362f502cd682c6a06859bc5b1f93da72b4be
LaraCL <152405898+LaraCL@users.noreply.github.com>
2024-05-03 19:16:23 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,9 +35,11 @@ function setup() {
 
 function gotFile(file) {
   if (file.type === 'image') {
-    // Das Bild als p5.Element-Objekt erstellen
+    // Das Bild als p5.Element-Objekt erstellen und im Drag-and-Drop-Feld anzeigen
     img = createImg(file.data, 'Uploaded Image', '', () => {
       img.hide(); // Bild vorübergehend ausblenden
+      img.size(200, 200); // Größe festlegen
+      img.parent('drop_zone'); // Bild in #drop_zone-Bereich einfügen
     });
   } else {
     console.log('Es wurde keine Bilddatei hochgeladen.');
@@ -59,13 +61,6 @@ function gotResult(error, results) {
     // Ergebnis anzeigen
     resultDiv.html(''); // Vorheriges Ergebnis löschen
 
-    // Thumbnail-Anzeige
-    let thumbnailElement = select('#thumbnail');
-    thumbnailElement.html(''); // Vorhandenes Inhalt löschen
-    img.show(); // Bild sichtbar machen
-    img.size(200, 200); // Größe festlegen
-    img.parent('thumbnail'); // Bild in #thumbnail-Bereich einfügen
-
     // Ergebnis in Tabellenform anzeigen
     let resultTable = createTable();
     resultTable.addColumn('string');
@@ -73,9 +68,9 @@ function gotResult(error, results) {
     resultTable.addColumn('number');
 
     let row = resultTable.addRow();
-    row.setString(0, `<img src="${img.elt.src}" width="100">`);
-    row.setString(1, results[0].label);
-    row.setNum(2, results[0].confidence);
+    row.addCell(`<img src="${img.elt.src}" width="100">`);
+    row.addCell(results[0].label);
+    row.addCell(nf(results[0].confidence, 0, 2));
 
     resultDiv.child(resultTable);
     
